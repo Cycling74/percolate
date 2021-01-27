304bdf7d22d08962fce130e28eae81ade9493932
Isabel Kaspriskie <isabel@cycling74.com>
2021-01-27 13:58:42 -0600

    terrain~: Rewrite perform64 to actually function

--- source/projects/terrain~/terrain~.c
+++ source/projects/terrain~/terrain~.c
@@ -8,7 +8,7 @@
 #include "z_dsp.h"
 #include "buffer.h"
 
-#define MAXFRAMES 32
+#define MAXFRAMES 64
 
 t_class *terrain_class;
 
@@ -18,18 +18,12 @@ typedef struct _terrain
     t_buffer_ref *l_buffer;
     long l_framesize;
     long l_numframes;
-    float l_hopsize;
 } t_terrain;
-
-float FConstrain(float v, float lo, float hi);
-long IConstrain(long v, long lo, long hi);
 
 void *terrain_new(t_symbol *s, long n, long f);
 void terrain_free(t_terrain *x);
 t_max_err terrain_notify(t_terrain *x, t_symbol *s, t_symbol *msg, void *sender, void *data);
 void terrain_assist(t_terrain *x, void *b, long m, long a, char *s);
-
-// dsp stuff
 void terrain_dsp64(t_terrain *x, t_object *dsp64, short *count, double samplerate, long maxvectorsize, long flags);
 void terrain_perform64(t_terrain *x, t_object *dsp64, double **ins, long numins, double **outs, long numouts, long sampleframes, long flags, void *userparam);
 
@@ -39,25 +33,6 @@ void terrain_frames(t_terrain *x, long frames);
 
 t_symbol *ps_buffer;
 
-long IConstrain(long v, long lo, long hi)
-{
-	if (v < lo)
-		return lo;
-	else if (v > hi)
-		return hi;
-	else
-		return v;
-}
-
-float FConstrain(float v, float lo, float hi)
-{
-	if (v < lo)
-		return lo;
-	else if (v > hi)
-		return hi;
-	else
-		return v;
-}
 
 void ext_main(void* p)
 {
@@ -83,12 +58,22 @@ void *terrain_new(t_symbol *s, long n, long f)
 	dsp_setup((t_pxobject *)x, 2);
 	outlet_new((t_object *)x, "signal");
 	terrain_set(x, s);
-	x->l_framesize = 512;
-	x->l_numframes = 2;
-	if(n) x->l_framesize = n;
-	if(f) x->l_numframes = IConstrain(f, 2, MAXFRAMES);
-	x->l_hopsize = 1./(float)(x->l_numframes-1);
-	return (x);
+
+	// Single frame size (samples)
+	if (n) {
+		x->l_framesize = n;
+	} else {
+		x->l_framesize = 512;
+	}
+
+	// Number of frames
+	if (f) {
+		x->l_numframes = CLAMP(f, 2, MAXFRAMES);
+	} else {
+		x->l_numframes = 2;
+	}
+
+	return x;
 }
 
 
@@ -111,17 +96,17 @@ void terrain_assist(t_terrain *x, void *b, long m, long a, char *s)
 		case 1: // inlet
 			switch(a) {
 				case 0:
-                    sprintf(s, "(signal) X Index");
+                    sprintf(s, "(signal) x-index");
                     break;
 				case 1:
-                    sprintf(s, "(signal) Y Index");
+                    sprintf(s, "(signal) y-index");
                     break;
 			}
             break;
 		case 2: // outlet
 			switch(a) {
 				case 0:
-                    sprintf(s, "(signal) Z Output");
+                    sprintf(s, "(signal) z-output");
                     break;
 			}
             break;
@@ -137,23 +122,18 @@ void terrain_set(t_terrain *x, t_symbol *s)
 		buffer_ref_set(x->l_buffer, s);
 }
 
+
 void terrain_size(t_terrain *x, long size)
 {
-    
-    if(size<1) size = 1;
+    if (size < 1) size = 1;
     x->l_framesize = size;
-    
 }
+
 
 void terrain_frames(t_terrain *x, long frames)
 {
-    
-    if(frames<2) frames = 2;
-    if(frames>MAXFRAMES) frames = MAXFRAMES;
+	frames = CLAMP(frames, 2, MAXFRAMES);
     x->l_numframes=frames;
-    x->l_hopsize = 1./(float)(x->l_numframes-1);
-    
-    
 }
 
 
@@ -162,60 +142,64 @@ void terrain_dsp64(t_terrain *x, t_object *dsp64, short *count, double samplerat
     object_method(dsp64, gensym("dsp_add64"), x, terrain_perform64, 0, NULL);
 }
 
+
 void terrain_perform64(t_terrain *x, t_object *dsp64, double **ins, long numins, double **outs, long numouts, long sampleframes, long flags, void *userparam)
 {
-    t_double *inx = (t_double *)(ins[0]);
-    t_double *iny = (t_double *)(ins[1]);
-    t_double *out = (t_double *)(outs[0]);
+    t_double *inx = ins[0];
+    t_double *iny = ins[1];
+    t_double *out = outs[0];
     
     int n = sampleframes;
-	float *tab;
-	float x_coords, y_coords, hopsize;
-	long index, index2, frames,nc,fsize, nframes;
-	register short p,q;
+
 	t_buffer_obj *buffer = buffer_ref_getobject(x->l_buffer);
-	
-	tab = buffer_locksamples(buffer);
+	float *tab = buffer_locksamples(buffer);
 	if (!tab)
 		goto zero;
+
+	long buffer_size = buffer_getframecount(buffer);
+	long nc = buffer_getchannelcount(buffer);
+	long fsize = x->l_framesize;
+	long num_frames = x->l_numframes;
+
+	float x_coord, y_coord;
+	long index, index2;
+    long frame;
+    float frac;
 	
-	frames = buffer_getframecount(buffer);
-	nc = buffer_getchannelcount(buffer);
-	fsize=x->l_framesize;
-	nframes = x->l_numframes;
-	hopsize = x->l_hopsize;
-	while (--n) {
-		x_coords = *++inx;
-		y_coords = *++iny;
-		if (y_coords<0.) y_coords = 0.;
-		if (y_coords>1.0) y_coords = 1.0; // constrain y coordinates to 0 to 1
-		index = x_coords*fsize;
-		if (index<0) index = 0;
-		if (index>=fsize) index = fsize-1; // constrain index coordinates to the frame boundaries
-		// check buffer~ boundaries
-		if (index >= frames)
-			index = frames - 1;
-		if (index+((nframes-1)*fsize) >= frames) // out of bounds
-			goto zero;
-		// figure out which two frames we're between...
-		q=0;
-		while (++q<nframes) {
-			if (y_coords<=(hopsize*q)) {
-				p = q-1;
-				q=nframes;
-			}
+	while (n--) {
+		// Location in single frame
+		x_coord = *inx++;
+		index = x_coord * fsize;
+		index = CLAMP(index, 0, fsize - 1);
+
+		if (index + ((num_frames - 1) * fsize) >= buffer_size) {
+			goto zero;  // Out of bounds of buffer
 		}
-		index = index+(p*fsize); // current frame
-		index2 = index+fsize; // one frame later
-		if (nc > 1) {
-			index = index * nc;
-			index2 = index2 * nc;
-		}
-		y_coords = (y_coords*(nframes-1))-p;
-		*++out = ((1.-y_coords)*tab[index])+(y_coords*tab[index2]); // this is wrong -- y_coords amp isn't scaling yet.
+
+		// Location in terrain (which frame?)
+		y_coord = CLAMP(*iny++, 0.0, 1.0);
+        frame = (num_frames - 1) * y_coord;  // Integer part of the terrain location (lower frame)
+        frac = (num_frames - 1) * y_coord - frame;  // Fractional part
+
+		// Get the frame index in the frame above and below the y-coord
+		index = index + (frame * fsize);  // Current frame
+		index2 = index + fsize;  // One frame later
+
+		// Scale by the number of channels of audio
+		index = index * nc;
+		index2 = index2 * nc;
+        
+        // Make sure we don't go out of bounds
+        index = CLAMP(index, 0, buffer_size - 1);
+        index2 = CLAMP(index, 0, buffer_size - 1);
+
+		// Interpolate between tab[index] and tab[index2]
+		*out++ = ((1.0 - frac) * tab[index]) + (frac * tab[index2]);
 	}
+
 	buffer_unlocksamples(buffer);
 	return;
+
 zero:
 	while (--n) *++out = 0.;
     return;
