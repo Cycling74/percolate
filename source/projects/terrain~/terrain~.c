c6058564864acca942f89a4caa9ad749a89bebf9
Isabel Kaspriskie <isabel@cycling74.com>
2021-01-27 16:18:44 -0600

    terrain~: Further fix algorithm

--- source/projects/terrain~/terrain~.c
+++ source/projects/terrain~/terrain~.c
@@ -1,7 +1,8 @@
 // terrain~ -- interpolates between two or more wavetable 'frames' stored in a buffer.
 //
 // updated for Max 7 by Darwin Grosse and Tim Place
-// ------------------------------------------------
+// updated algorithm and support for Max 8 by Isabel Kaspriskie
+// ------------------------------------------------------------
 
 #include "ext.h"
 #include "ext_obex.h"
@@ -14,10 +15,10 @@ t_class *terrain_class;
 
 typedef struct _terrain
 {
-    t_pxobject l_obj;
-    t_buffer_ref *l_buffer;
-    long l_framesize;
-    long l_numframes;
+	t_pxobject l_obj;
+	t_buffer_ref *l_buffer;
+	long l_framesize;
+	long l_numframes;
 } t_terrain;
 
 void *terrain_new(t_symbol *s, long n, long f);
@@ -36,19 +37,19 @@ t_symbol *ps_buffer;
 
 void ext_main(void* p)
 {
-    t_class *c = class_new("terrain~", (method)terrain_new, (method)terrain_free, (long)sizeof(t_terrain), 0L, A_SYM, A_DEFLONG, A_DEFLONG, 0);
-    
-    class_addmethod(c, (method)terrain_assist, "assist", A_CANT, 0);
-    class_addmethod(c, (method)terrain_dsp64, "dsp64", A_CANT, 0);
+	t_class *c = class_new("terrain~", (method)terrain_new, (method)terrain_free, (long)sizeof(t_terrain), 0L, A_SYM, A_DEFLONG, A_DEFLONG, 0);
+	
+	class_addmethod(c, (method)terrain_assist, "assist", A_CANT, 0);
+	class_addmethod(c, (method)terrain_dsp64, "dsp64", A_CANT, 0);
 	class_addmethod(c, (method)terrain_notify, "notify", A_CANT, 0);
  
 	class_addmethod(c, (method)terrain_set, "set", A_SYM, 0);
 	class_addmethod(c, (method)terrain_size, "size", A_DEFLONG, 0);
 	class_addmethod(c, (method)terrain_frames, "frames", A_DEFLONG, 0);
-    class_dspinit(c);
-    
-    class_register(CLASS_BOX, c);
-    terrain_class = c;
+	class_dspinit(c);
+	
+	class_register(CLASS_BOX, c);
+	terrain_class = c;
 	ps_buffer = gensym("buffer~");
 }
 
@@ -96,22 +97,22 @@ void terrain_assist(t_terrain *x, void *b, long m, long a, char *s)
 		case 1: // inlet
 			switch(a) {
 				case 0:
-                    sprintf(s, "(signal) x-index");
-                    break;
+					sprintf(s, "(signal) x-index");
+					break;
 				case 1:
-                    sprintf(s, "(signal) y-index");
-                    break;
+					sprintf(s, "(signal) y-index");
+					break;
 			}
-            break;
+			break;
 		case 2: // outlet
 			switch(a) {
 				case 0:
-                    sprintf(s, "(signal) z-output");
-                    break;
+					sprintf(s, "(signal) z-output");
+					break;
 			}
-            break;
+			break;
 	}
-    
+	
 }
 
 void terrain_set(t_terrain *x, t_symbol *s)
@@ -125,31 +126,31 @@ void terrain_set(t_terrain *x, t_symbol *s)
 
 void terrain_size(t_terrain *x, long size)
 {
-    if (size < 1) size = 1;
-    x->l_framesize = size;
+	if (size < 1) size = 1;
+	x->l_framesize = size;
 }
 
 
 void terrain_frames(t_terrain *x, long frames)
 {
 	frames = CLAMP(frames, 2, MAXFRAMES);
-    x->l_numframes=frames;
+	x->l_numframes=frames;
 }
 
 
 void terrain_dsp64(t_terrain *x, t_object *dsp64, short *count, double samplerate, long maxvectorsize, long flags)
 {
-    object_method(dsp64, gensym("dsp_add64"), x, terrain_perform64, 0, NULL);
+	object_method(dsp64, gensym("dsp_add64"), x, terrain_perform64, 0, NULL);
 }
 
 
 void terrain_perform64(t_terrain *x, t_object *dsp64, double **ins, long numins, double **outs, long numouts, long sampleframes, long flags, void *userparam)
 {
-    t_double *inx = ins[0];
-    t_double *iny = ins[1];
-    t_double *out = outs[0];
-    
-    int n = sampleframes;
+	t_double *inx = ins[0];
+	t_double *iny = ins[1];
+	t_double *out = outs[0];
+	
+	int n = sampleframes;
 
 	t_buffer_obj *buffer = buffer_ref_getobject(x->l_buffer);
 	float *tab = buffer_locksamples(buffer);
@@ -158,43 +159,61 @@ void terrain_perform64(t_terrain *x, t_object *dsp64, double **ins, long numins,
 
 	long buffer_size = buffer_getframecount(buffer);
 	long nc = buffer_getchannelcount(buffer);
-	long fsize = x->l_framesize;
+	long framesize = x->l_framesize;
 	long num_frames = x->l_numframes;
-
-	float x_coord, y_coord;
-	long index, index2;
-    long frame;
-    float frac;
+	long table_size = framesize * num_frames;
+	table_size = CLAMP(table_size, 1, buffer_size);
 	
+	// To get the sample, we have to interpolate in two dimensions:
+	//     1) within the frame itself
+	//     2) between frames in the table
+	// Terminology is as follows:
+	//
+	//     table top, frame left |-------------| table top, frame right
+	//                           |             |
+	//                           |             |
+	//                           |             |
+	//  table bottom, frame left |-------------| table bottom, frame right
+	float frame_pos, table_pos, terrain_pos;
+	float frame_pos_frac, table_pos_frac;
+	float bottom_left, bottom_right, top_left, top_right;
+	float above, below;
+	int frame_pos_left, frame_pos_right;
+	int table_pos_bottom, table_pos_top;
+
 	while (n--) {
-		// Location in single frame
-		x_coord = *inx++;
-		index = x_coord * fsize;
-		index = CLAMP(index, 0, fsize - 1);
+		frame_pos = *inx++ * framesize;
+		terrain_pos = *iny++;
 
-		if (index + ((num_frames - 1) * fsize) >= buffer_size) {
-			goto zero;  // Out of bounds of buffer
+		// Position in the frame
+		frame_pos_left = (int) frame_pos;
+		frame_pos_right = 0;
+		if (frame_pos_left == framesize - 1) {
+			// End of frame, so wrap to beginning
+			frame_pos_right = (int) 0;
+		} else {
+			frame_pos_right = frame_pos_left + 1;
 		}
+		frame_pos_frac = frame_pos - (float) frame_pos_left;
 
-		// Location in terrain (which frame?)
-		y_coord = CLAMP(*iny++, 0.0, 1.0);
-        frame = (num_frames - 1) * y_coord;  // Integer part of the terrain location (lower frame)
-        frac = (num_frames - 1) * y_coord - frame;  // Fractional part
+		// Position in table
+		table_pos = terrain_pos * (num_frames - 1);  // scale from [0..1] to [0..table_size - 1]
+		table_pos_bottom = floor(table_pos);
+		table_pos_top = ceil(table_pos);
+		table_pos_frac = table_pos - (float) table_pos_bottom;  // [0..1]
 
-		// Get the frame index in the frame above and below the y-coord
-		index = index + (frame * fsize);  // Current frame
-		index2 = index + fsize;  // One frame later
+		// Look up nearby samples
+		bottom_left = tab[table_pos_bottom * framesize + frame_pos_left];
+		bottom_right = tab[table_pos_bottom * framesize + frame_pos_right];
+		top_left = tab[table_pos_top * framesize + frame_pos_left];
+		top_right = tab[table_pos_top * framesize + frame_pos_right];
 
-		// Scale by the number of channels of audio
-		index = index * nc;
-		index2 = index2 * nc;
-        
-        // Make sure we don't go out of bounds
-        index = CLAMP(index, 0, buffer_size - 1);
-        index2 = CLAMP(index, 0, buffer_size - 1);
+		// Interpolate within both of the two above and below frames
+		above = bottom_left + frame_pos_frac * (bottom_right - bottom_left);
+		below = top_left + frame_pos_frac * (top_right - top_left);
 
-		// Interpolate between tab[index] and tab[index2]
-		*out++ = ((1.0 - frac) * tab[index]) + (frac * tab[index2]);
+		// Interpolate between frames and return
+		*out++ = below + table_pos_frac * (above - below);
 	}
 
 	buffer_unlocksamples(buffer);
@@ -202,5 +221,5 @@ void terrain_perform64(t_terrain *x, t_object *dsp64, double **ins, long numins,
 
 zero:
 	while (--n) *++out = 0.;
-    return;
+	return;
 }
